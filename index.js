f8846b14c63beefd68c9c387e405ebe7e4ca3547
BrunaGbispo <bruna.araujobispo@gmail.com>
2024-05-17 00:00:56 -0300

    misericordia

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const server = express();
+const port = 3000;
 const livros = require('./src/data/livros.json');
 
 server.get('/', (req,res) =>{
     return res.json(livros)
 })
 
-server.listen(3000, () =>{
+server.listen(port, () =>{
     console.log('Servidor está funcionando :)')
 })
